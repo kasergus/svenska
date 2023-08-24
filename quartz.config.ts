b0b2f42d67c23e81dfb0ec39714409bc03944fce
kasergus <kasergus@gmail.com>
2023-08-24 23:42:57 +0200

    some configs was edited

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "📋 Svenska conspect",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
