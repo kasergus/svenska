596dcf9406b8de86a4ba31798e6e80b5848d25db
kasergus <kasergus@gmail.com>
2023-09-07 00:42:33 +0200

    svenska_token test

--- quartz.config.ts
+++ quartz.config.ts
@@ -31,7 +31,7 @@ const config: QuartzConfig = {
         darkMode: {
           light: "#24273a",  // base 
           lightgray: "#494d64",  // surface1 
-          gray: "#cad3f4",
+          gray: "#cad3f5",
           darkgray: "#cad3f5",  // text
           dark: "#cad3f5", 
           secondary: "#8aadf4",
